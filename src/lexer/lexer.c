b104f31e85a259797355d3efc6a396b040a49443
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-05-09 17:13:53 -0400

    basic identifier token working

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -2,10 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <regex.h>
+#include <ctype.h>
 #include "lexer.h"
 #include "../helpers.h"
 
-token *append(token *head, char *src, int type)
+static FILE *file_stream;
+
+static void nav_back(long offset)
+{
+    fseek(file_stream, offset, SEEK_CUR);
+}
+
+static token *append(token *head, char *src, TokenType type)
 {
     token *cursor;
     token *new_tk = malloc(sizeof(token));
@@ -30,12 +38,42 @@ token *append(token *head, char *src, int type)
     return new_tk;
 }
 
+TokenType get_ident(token *head)
+{
+    regex_t ascii_regex;
+    regcomp(&ascii_regex, ASCII, 0);
+    char buf[2] = "\0";
+
+    // todo make buffer helper funcs like expand and stuff
+    char identifier[256];
+    for (buf[0] = next_ch(); regexec(&ascii_regex, buf, 0, NULL, 0) == 0; buf[0] = next_ch())
+    {
+        strcat(identifier, buf);
+    }
+    regfree(&ascii_regex);
+    append(head, identifier, ident);
+
+    // go back 1 char in file because the for loop will read an
+    // extra char that the main lex loop should read
+    nav_back(-1L);
+}
+
+int next_ch()
+{
+    return fgetc(file_stream);
+}
+
 void lex(FILE *cpl_file)
 {
+    file_stream = cpl_file;
+
+    regex_t numbers;
+
+    regcomp(&numbers, NUMBER, 0);
     token *head = malloc(sizeof(token));
     char buf[2] = "\0";
 
-    while ((buf[0] = fgetc(cpl_file)) != EOF)
+    while ((buf[0] = next_ch()) != EOF)
     {
         if (strstr(SCOL, buf))
             continue;
@@ -45,10 +83,21 @@ void lex(FILE *cpl_file)
             append(head, buf, lpar);
         else if (strstr(RPAR, buf))
             append(head, buf, rpar);
-        else if (strstr(PLUS, buf))
-            append(head, buf, plus);
+        else if (regexec(&numbers, buf, 0, NULL, 0) == 0)
+            append(head, buf, number);
+        else
+        {
+            // go back 1 char in file because the loop will read an
+            // extra char that the ident loop should read
+            nav_back(-1L);
+            get_ident(head);
+        }
     };
 
+    fclose(file_stream);
+
     for (; head != NULL; head = head->next)
-        printf(head->value);
+    {
+        printf("token value: %s\n", head->value);
+    }
 }
--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,25 +1,31 @@
-enum
+typedef enum
 {
     scol,
     number,
-    prnt,
+    ident,
     lpar,
     rpar,
     plus,
-};
+} TokenType;
 
 #define SCOL ";"
-#define NUMBER "^[0-9]+$"
+#define NUMBER "^[0-9]$"
 #define PRNT "print"
 #define LPAR "("
 #define RPAR ")"
 #define PLUS "+"
+#define NEWLINE "\n"
+#define ASCII "[a-zA-Z]"
 
 typedef struct _token
 {
-    int type;
+    TokenType type;
     char *value;
     struct _token *next;
 } token;
 
+static void nav_back(long offset);
+static token *append(token *head, char *src, TokenType type);
+TokenType get_ident(token *head);
+int next_ch();
 void lex(FILE *cpl_file);
